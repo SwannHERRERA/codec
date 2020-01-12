c7a2dfd8faea9e95b8292cbe54a15bce46b41b9e
Swann <swann.herrera@gmail.com>
2020-01-12 20:27:14 +0100

    commit ça marche final

--- index.js
+++ index.js
@@ -5,152 +5,136 @@ const inquirer = require('inquirer');
 const key_helper = require('./key.js');
 const utils_helper = require('./utils_helper.js');
 
-convert_binary = (integer) => integer.toString(2).padStart(8, '0').split('');
+convert_binary = (integer) => integer.toString(2).padStart(8, '0');
 
-binaryArrayToInt = (array_of_array) => {
-    let array_of_int = [];
-    for (let index = 0; index < array_of_array.length; index += 1) {
-        array_of_int[index] = 0;
-        for (let i = 0; i < array_of_array[index].length; i += 1) {
-            if (array_of_array[index][i]) {
-                array_of_int[index] += 2 ** (7 - i);
-            }
-        }
-    }
-    return new Uint8Array(array_of_int); // We use Uint8Array beacause it's necesary for writing file with nodejs
-}
-
-getFileAsBinary = (path) => {
-    const binary_file = [];
-    let file = fs.readFileSync(path, (err, data) => {
-        if (err) throw err;
-    });
-    file.forEach((char, i) => {
-        binary_file[i] = convert_binary(char);
-    });
-    return binary_file;
-}
-
-compressFile = (file, key, path_to_new_file) => {
-    let writeStream = fs.createWriteStream(path_to_new_file);
-    file = file.flat().join('');
-
-    let regex = new RegExp('.{1,' + utils_helper.IDENTITY_MATRICE_LENGTH + '}', 'g');
-    file = file.match(regex);
-    file.forEach((group_of_bits) => {
-        tmp = [];
-        for (let i = 0; i < utils_helper.LENGTH_REFERENCE; i += 1) {
-            tmp[i] = 0;
-        }
-        for (let j = 0; j < group_of_bits.length; j += 1) {
-            if (group_of_bits[j] === '1') {
-                for (let k = 0; k < key[j].length; k += 1) {
-                    tmp[k] = Number(key[j][k]) + Number(tmp[k]);
-                }
-            }
-        }
-        for (let i = 0; i < tmp.length; i += 1) {
-            tmp[i] = tmp[i] % 2;
-        }
-        /**
-         * faire le truc de manière sync
-         */
-        writeStream.write(bi_to_int(tmp), 'binary');
-    });
-    writeStream.end();
+binary_to_int = (binary) => {
+	let sum = Number();
+	for (let i = 0; i < binary.length; i += 1) {
+		sum += binary[i] * 2 ** i;
+	}
+	return new Uint8Array([ sum ]);
 };
 
-bi_to_int = (binary) => {
-    let sum = Number();
-    for (let i = 0; i < binary.length; i += 1) {
-        sum += binary[i] * (2 ** i);
-    }
-    return new Uint8Array([sum]);
-}
+init_array = (nb_of_element) => {
+	result = [];
+	for (let i = 0; i < nb_of_element; i += 1) {
+		result[i] = 0;
+	}
+	return result;
+};
+
+NewCompressFile = (path, key) => {
+	let readStream = fs.createReadStream(path, { encoding: null });
+	const fd = fs.openSync(path + '.cry', 'w');
+	let regex = new RegExp('.{1,' + utils_helper.IDENTITY_MATRICE_LENGTH + '}', 'g');
+	readStream.on('data', (chunk) => {
+		if (chunk !== null) {
+			chunk.forEach((integer) => {
+				let binary_string = convert_binary(integer);
+				binary_string = binary_string.match(regex);
+				binary_string.forEach((group_of_bits) => {
+					let result = init_array(utils_helper.LENGTH_REFERENCE);
+					for (let i = 0; i < group_of_bits.length; i += 1) {
+						if (group_of_bits[i] === '1') {
+							for (let j = 0; j < key[i].length; j += 1) {
+								if (key[i][j] === '1') {
+									if (result[j] === 1) {
+										result[j] = 0;
+									} else {
+										result[j] = 1;
+									}
+								}
+							}
+						}
+					}
+					fs.writeSync(fd, binary_to_int(result.reverse()), null, null);
+				});
+			});
+		}
+	});
+	readStream.on('end', () => {
+		console.log('end of reading');
+	});
+};
 
 makeOctets = (array_of_bit) => {
-    let new_array = new Array(Math.ceil(array_of_bit.length / 8));
-    for (let i = 0; i < new_array.length; i += 1) {
-        new_array[i] = [];
-    }
-    let compteur_octet = 0;
-    let compteur_bit = 0;
-    for (let i = 0; i < array_of_bit.length; i += 1) {
-        new_array[compteur_octet][compteur_bit] = array_of_bit[i];
-        compteur_bit += 1;
-        if (compteur_bit >= 8) {
-            compteur_bit = 0;
-            compteur_octet += 1;
-        }
-    }
-    if (new_array[compteur_octet] != undefined) {
-        throw new Error('Taille de clé incorrect');
-    }
+	let new_array = new Array(Math.ceil(array_of_bit.length / 8));
+	for (let i = 0; i < new_array.length; i += 1) {
+		new_array[i] = [];
+	}
+	let compteur_octet = 0;
+	let compteur_bit = 0;
+	for (let i = 0; i < array_of_bit.length; i += 1) {
+		new_array[compteur_octet][compteur_bit] = array_of_bit[i];
+		compteur_bit += 1;
+		if (compteur_bit >= 8) {
+			compteur_bit = 0;
+			compteur_octet += 1;
+		}
+	}
+	if (new_array[compteur_octet] != undefined) {
+		throw new Error('Taille de clé incorrect');
+	}
+	for (let i = 0; i < new_array.length; i += 1) {
+		new_array[i] = binary_to_int(new_array[i].reverse());
+	}
+	return new Uint8Array(new_array);
+};
 
-    return new_array;
-}
+unCompressFile = (path, identity_matrice) => {
+	let readStream = fs.createReadStream(path + '.cry', { encoding: null });
+	let result = [];
+	readStream.on('data', (chunk) => {
+		const fd = fs.openSync(path, 'w');
+		if (chunk !== null) {
+			chunk.forEach((integer) => {
+				const ocetets = convert_binary(integer);
+				identity_matrice.forEach((number) => {
+					result.push(Number(ocetets[number]));
+				});
+			});
+			const results = makeOctets(result);
+			fs.writeSync(fd, results, null, null);
+		}
+		fs.closeSync(fd);
+	});
+	readStream.on('end', () => {
+		console.log('end of reading');
+	});
+};
 
-writeFile = (path, data) => {
-    const buffer = Buffer.alloc(data.length, data, 'binary');
-    fs.open(path, 'w', function (err, fd) {
-        if (err) {
-            throw 'error opening file: ' + err;
-        }
-        // console.log(buffer);
-        fs.write(fd, buffer, 0, buffer.length, null, function (err) {
-            if (err) throw 'error writing file: ' + err;
-            fs.close(fd, function () {
-                console.log('file written');
-            })
-        });
-    });
-}
-
-uncompressFile = (compressed_file, identity_matrice, path_to_new_file) => {
-    let result = [];
-    let writeStream = fs.createWriteStream(path_to_new_file);
-    for (index = 0; index < compressed_file.length; index += utils_helper.LENGTH_REFERENCE) {
-        identity_matrice.forEach(number => {
-            result.push(Number(compressed_file[index + number]));
-        });
-        if (result.length % 8 === 0) {
-            writeStream.write(bi_to_int(result), 'binary');
-            result = [];
-        }
-    }
-    writeStream.end();
-}
-
-
-
-inquirer.prompt([{
-    type: 'list',
-    name: 'action',
-    message: 'What do you want to do ?',
-    choices: ['chiffrer', 'déchiffer'],
-},
-]).then(answers_1 => {
-    inquirer.prompt([
-            {
-                name: 'path',
-                message: 'what\'s the path to your file ?',
-                default:  './image.jpg',
-            },
-        ]).then(answers_2 => {
-            if (answers_1.action == 'chiffrer') { // mode chiffrement
-                console.time("chiffrement");
-                const key = key_helper.getKey('./key4.txt');
-                const file = getFileAsBinary(answers_2.path);
-                const compressed_file = compressFile(file, key, answers_2.path + '.cry');
-                console.timeEnd("chiffrement");
-            } else { // mode déchiffrement
-                console.time("déchiffrement");
-                const key = key_helper.getKey('./key4.txt');
-                const identity_matrice = key_helper.getIidentityMatrice(key);
-                console.log(identity_matrice);
-                const compressed_file = getFileAsBinary(answers_2.path + '.cry');
-                uncompressFile(compressed_file.flat(), identity_matrice, answers_2.path);
-                console.timeEnd("déchiffrement");
-            }
-        });
-});
+inquirer
+	.prompt([
+		{
+			type: 'list',
+			name: 'action',
+			message: 'What do you want to do ?',
+			choices: [ 'chiffrer', 'déchiffer' ]
+		}
+	])
+	.then((answers_1) => {
+		inquirer
+			.prompt([
+				{
+					name: 'path',
+					message: "what's the path to your file ?",
+					default: './test.txt'
+				}
+			])
+			.then((answers_2) => {
+				if (answers_1.action == 'chiffrer') {
+					// mode chiffrement
+					console.time('chiffrement');
+					const key = key_helper.getKey('./key4.txt');
+					NewCompressFile(answers_2.path, key);
+					console.timeEnd('chiffrement');
+				} else {
+					// mode déchiffrement
+					console.time('déchiffrement');
+					const key = key_helper.getKey('./key4.txt');
+					const identity_matrice = key_helper.getIidentityMatrice(key);
+					unCompressFile(answers_2.path, identity_matrice);
+					console.timeEnd('déchiffrement');
+				}
+			});
+	});
